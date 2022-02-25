63990b353c73864051657af027df0e3215efb98b
Wei Wei <weiweibcm@gmail.com>
2022-02-24 20:40:01 -0800

    partI and II updated after code review

--- melons.py
+++ melons.py
@@ -3,23 +3,24 @@
 
 class AbstractMelonOrder:
     
-    order_type = None
+    
     tax = 0
     shipped = False
-    flat_fee = 0
+    
     
 
-    def __init__(self, species, qty):
+    def __init__(self, order_type, species, qty):
         self.species = species
         self.qty = qty
         self.base_price = 5
+        self.order_type = order_type
         if species == 'Christmas melon':
             self.base_price = 1.5 * self.base_price
 
     def get_total(self):
         """Calculate price, including tax."""
         
-        total = (1 + self.tax) * self.qty * self.base_price + self.flat_fee
+        total = (1 + self.tax) * self.qty * self.base_price
         return round(total, 4)
 
     def mark_shipped(self):
@@ -32,31 +33,38 @@ class AbstractMelonOrder:
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
     
-    order_type = "domestic"
+    
     tax = 0.08
-    country_code = 'US'
+    
 
 
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
-        super().__init__(species, qty)
+        super().__init__('domestic', species, qty)
         
-   
 
  
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
     
-    order_type = "international"
+    
     tax = 0.17
     
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
-        super().__init__(species, qty)        
+        super().__init__('international', species, qty)        
         self.country_code = country_code
+        # if qty < 10:
+        #     self.flat_fee = 3
+    
+    def get_total(self):
+        """Calculate price, including tax."""
+        total = super().get_total()
         if qty < 10:
-            self.flat_fee = 3
+            total += 3
+        return total
+        
           
     def get_country_code(self):
         """Return the country code."""
